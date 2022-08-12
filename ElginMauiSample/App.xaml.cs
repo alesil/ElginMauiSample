1fc40c4607ff63be396fcfc44c13fdce611ca09d
Alexandre Rodrigues da Silva <alexandre@alesil.com.br>
2022-08-12 11:24:32 -0300

    Add project files.

---
+++ ElginMauiSample/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace ElginMauiSample;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ ElginMauiSample/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ElginMauiSample;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
